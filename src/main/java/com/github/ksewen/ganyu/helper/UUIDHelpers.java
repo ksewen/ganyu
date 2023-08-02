19a121c44ad08a7deea36f9f715697fd77446e53
ksewen <261517946@qq.com>
2023-08-02 12:06:02 +0300

    add tests for classes in the package com.github.ksewen.ganyu.helper and fix bugs.

--- src/main/java/com/github/ksewen/ganyu/helper/UUIDHelpers.java
+++ src/main/java/com/github/ksewen/ganyu/helper/UUIDHelpers.java
@@ -9,35 +9,35 @@ import org.springframework.util.StringUtils;
  */
 public class UUIDHelpers {
 
-    public String getShortUUID(String uuid) {
-        if (!isValidUUID(uuid)) {
-            return UUID.randomUUID().toString().replace("-", "");
-        }
-        return uuid.replace("-", "");
+  public String getShortUUID(String uuid) {
+    if (!isValidUUID(uuid)) {
+      return UUID.randomUUID().toString().replace("-", "");
     }
+    return uuid.replace("-", "");
+  }
 
-    public String getShortUUID() {
-        return UUID.randomUUID().toString().replace("-", "");
-    }
+  public String getShortUUID() {
+    return UUID.randomUUID().toString().replace("-", "");
+  }
 
-    public String getRandomUUID() {
-        return getRandomUUID(null);
-    }
+  public String getRandomUUID() {
+    return getRandomUUID(null);
+  }
 
-    public String getRandomUUID(String str) {
-        if (!StringUtils.hasLength(str)) {
-            return UUID.randomUUID().toString();
-        } else {
-            return UUID.nameUUIDFromBytes(str.getBytes()).toString();
-        }
+  public String getRandomUUID(String str) {
+    if (!StringUtils.hasLength(str)) {
+      return UUID.randomUUID().toString();
+    } else {
+      return UUID.nameUUIDFromBytes(str.getBytes()).toString();
     }
+  }
 
-    public static boolean isValidUUID(String uuid) {
-        if (!StringUtils.hasLength(uuid)) {
-            return false;
-        }
-        final String longPattern = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$";
-        final String shortPattern = "^[0-9a-f]{8}[0-9a-f]{4}[0-9a-f]{4}[0-9a-f]{4}[0-9a-f]{12}$";
-        return uuid.matches(longPattern) || uuid.matches(shortPattern);
+  public boolean isValidUUID(String uuid) {
+    if (!StringUtils.hasLength(uuid)) {
+      return false;
     }
+    final String longPattern = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$";
+    final String shortPattern = "^[0-9a-f]{8}[0-9a-f]{4}[0-9a-f]{4}[0-9a-f]{4}[0-9a-f]{12}$";
+    return uuid.matches(longPattern) || uuid.matches(shortPattern);
+  }
 }
